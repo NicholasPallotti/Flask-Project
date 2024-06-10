eeee29cf0383848949c8186cd1a595e1bb16d060
Nicholas Pallotti <tcnp1996@gmail.com>
2024-06-09 19:27:23 -0600

    added some new stuff

--- Main.py
+++ Main.py
@@ -1,7 +1,20 @@
 from flask import Flask
 from markupsafe import escape
+import pymongo
 
 app = Flask(__name__)
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+
+mydb = myclient["mydatabase"]
+mycol = mydb["Decks"]
+
+mylist = [
+      {"name": "amulet yitan", "format": "modern"},
+      {"name": "8-cast", "format": "legacy"},
+      {"name": "death and taxes", "format": "legacy"},
+]
+
+x = mycol.insert_many(mylist)
 
 @app.route("/")
 def hello_world():
@@ -14,3 +27,7 @@ def hello(name):
 @app.route('/projects/')
 def projects():
       return 'The project page'
+
+@app.route('/decks/')
+def decks():
+      return "this will eventually be a list of decks"
