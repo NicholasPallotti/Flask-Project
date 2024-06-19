52225db17c5976be0563bb6eca34e34e5877925f
Nicholas Pallotti <tcnp1996@gmail.com>
2024-06-19 00:06:25 -0600

    fixed bug in index html, in for loop instead of using d'deck' was using decks' causing the webpage to not load.

--- Main.py
+++ Main.py
@@ -6,18 +6,23 @@ app = Flask(__name__)
 
 client = MongoClient('localhost', 27017)
 
-db = client.mydatabase
+db = client.newdatabase
 decks = db.decks
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        deck_name = request.form['deck_name']
+        name = request.form['name']
         format = request.form['format']
-        decks.insert_one({'name':deck_name, 'format': format})
+        decks.insert_one({'name':name, 'format': format})
         return redirect(url_for('index'))
 
     all_decks = decks.find()
+    
+    #for x in all_decks:
+        #print(x)
+    print()
+    print()
     return render_template('index.html', decks=all_decks)
 
 @app.post("/<id>/delete/")
