27618fe25bdd93e978f4b7e8bf2eac6c712c6029
Nicholas Pallotti <tcnp1996@gmail.com>
2024-06-18 18:06:05 -0600

    aded index template base, changed default post request, added main method

--- Main.py
+++ Main.py
@@ -1,35 +1,20 @@
-from flask import Flask, request, render_template
-from flask_pymongo import PyMongo
-#import pymongo
-
-#myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-#mydb = myclient['testdatabase']
-#mycol = mydb["test"]
+from flask import Flask, url_for, request, render_template, redirect
+from pymongo import MongoCLient
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
-mongo = PyMongo(app)
 
+client = MongoClient('localhost', 27017)
+db = client.mydatabase
+decks = db.decks
 
-@app.route("/")
-def home_page():
-    online_users = mongo.db.users.find({"online": True})
-    return render_template("index.html",
-        online_users=online_users)
-#
-#@app.route("/")
-#def hello_world():
-#    return "<p>Hello, World!</p>"
-#
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    if request.method == 'POSt':
+        content = request.form['content']
 
-@app.get('/login')
-def login_get():
-     return 'get login'
+    all_decks = decks.find()
+    return render_template('index.html', decks=all_decks)
 
-@app.post('/login')
-def login_post():
-     return 'post login'
-
-@app.route('/decks/')
-def decks():
-      return
+if __name__ == "__main__":
+    app.run(debug=True)
